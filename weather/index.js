e3869e98fa0c880902eb316b6a83e4ae82a0f15e
abhyuday <abhyuday964808@gmail.com>
2018-08-21 06:38:37 +0530

    errorn msg

--- weather/index.js
+++ weather/index.js
@@ -20,6 +20,11 @@ const ar=yargs
 .argv;
 axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ar.address}`)
 .then((response)=>{
+    if(response.data.status==="ZERO_RESULTS"){
+        throw new Error("Address Not Found");
+        
+    }
+    
     const lat=response.data.results[0].geometry.location.lat;
     const lng=response.data.results[0].geometry.location.lng;
     const uri= `https://api.darksky.net/forecast/f6ba5641cc7962e78edab46fff541d90/${lat},${lng}`;
@@ -28,11 +33,15 @@ axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ar.addres
 })
 .then((response)=>{
    console.log("Current Temperature : ");
-   if(ar.t=='C'){
+   if(ar.t==='C'){
     const tem =Math.round((response.data.currently.temperature -32)*0.5555556);
     console.log(tem+" " + "C");}
     else
     console.log(response.data.currently.temperature+" F" );
 })
-.catch((error)=>{console.log("Error in connecting to API")});
-//console.log(ar);
+.catch((error)=>{
+    if(error.code==="ENOTFOUND")
+    console.log(error);
+        else
+     console.log(error.message)});
+
